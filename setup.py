7ead26431db1a5693234e9a297968a446751b6e7
AnsChaser <anschaser@163.com>
2023-09-11 18:07:08 +0800

    function: add base lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='aclib.builtins',
-    version='0.0.1',
+    version='0.0.2',
     author='AnsChaser',
     author_email='anschaser@163.com',
     description='extend functions for python builtins',
